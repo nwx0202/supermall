6ccf5ec43a4d144eb4e08e1ebe76eb9f74d509eb
nienie <765891612@qq.com>
2019-11-26 23:00:52 +0800

    引入css文件

--- src/App.vue
+++ src/App.vue
@@ -8,18 +8,11 @@
 export default {
   name: 'app',
   components: {
-    
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./assets/css/base.css";
 </style>
